0e16c8c75f8f35606d8dda6072d77c60d84afb03
ruitiari <gibsonruitiari@gmail.com>
2022-01-13 14:28:01 +0300

    added dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,7 +24,6 @@ dependencies {
     implementation("com.squareup.okhttp:logging-interceptor:2.7.5")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core-jvm:1.6.0-native-mt")
     implementation("com.squareup.moshi:moshi:1.13.0")
-    implementation ("org.apache.commons:commons-text:1.9")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
 
 
